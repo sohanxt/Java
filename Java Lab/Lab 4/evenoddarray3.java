725d5bdbaaf04283a3a5b965eb554535fe468028
ABU SAHID SOHAN <154542799+sohanxt@users.noreply.github.com>
2025-10-22 04:44:59 +0600

    Update evenoddarray3.java

--- Java Lab/Lab 4/evenoddarray3.java
+++ Java Lab/Lab 4/evenoddarray3.java
@@ -1,36 +1,58 @@
 import java.util.Scanner;
-public class evenoddarray3 {
-    public static void main (String [] args)    
+public class test3 {
+    public static void main (String [] args)
 {
+    Scanner sc = new Scanner(System.in);
 
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the number of elements: ");
-        int size = sc.nextInt();
-
-        int[] arr = new int[size];
-
-        System.out.println("Enter " + size + " integers:");
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        System.out.print("Even elements: ");
-        for (int i = 0; i < size; i++) {
-            if (arr[i] % 2 == 0) {
-                System.out.print(arr[i] + " ");
-            }
-        }
-
-        System.out.print("\nOdd elements: ");
-        for (int i = 0; i < size; i++) {
-            if (arr[i] % 2 != 0) {
-                System.out.print(arr[i] + " ");
-            }
-        }
-
-        sc.close();
+    System.out.println("Enter array size : ");
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+     int [] even = new int [n];
+     int[] odd = new int[n];
+     int evenindex = 0;
+     int oddindex = 0;
+    System.out.println("Enter array elements : ");
+    for(int i = 0 ; i < n ; i++)
+    {
+        arr[i] = sc.nextInt();
     }
-}
- 
+    sc.close();
+System.out.println("printing array elements : ");
 
+for(int i = 0 ; i < n ; i++)
+    {
+       System.out.print(" " +arr[i]);
+    }
+    System.out.println();
+
+    for (int i = 0 ; i < n ; i++)
+    {
+        if (arr[i]%2 == 0)
+        {
+            even[evenindex] = arr[i];
+            evenindex++;
+        }
+        else 
+        {
+            odd[oddindex] = arr[i];
+            oddindex++;
+        }
+    }
+
+    System.out.println("Printing even array :");
+for(int i = 0 ; i < evenindex; i++)
+    {
+       System.out.print(" " +even[i]);
+    }
+    System.out.println();
+  System.out.println("Printing odd array :");
+for(int i = 0 ; i <oddindex ; i++)
+    {
+       System.out.print(" " +odd[i]);
+    }
+    System.out.println();
+      
+
+}    
+}
